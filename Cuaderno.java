df45f21937c7e5c9343b38bb84309aa5b699777a
Brokolito <m.castro14@ufromail.cl>
2023-03-25 21:01:07 -0300

    Se hacen un par de cambios

--- Cuaderno.java
+++ Cuaderno.java
@@ -14,4 +14,25 @@ public class Cuaderno {
 		}
 		return hojas.size();
 	}
+	public int agregarHojas(Hojas hoja){
+		hojas.add(hoja);
+		return hojas.size();
+	}
+	public Hojas encontrarHojaPorTexto(String texto){
+		for(Hojas hojas1:this.hojas){
+			if (hojas1.getTexto().equals(texto)){
+				return hojas1;
+			}
+		}
+		return null;
+	}
+	public ArrayList<Hojas> encontrarHojasPorTipo(String tipo){
+		ArrayList<Hojas> hojasDelMismoTipo=new ArrayList<>();
+		for(Hojas hojas1:this.hojas){
+			if (hojas1.getTipo().equals(tipo)){
+				hojasDelMismoTipo.add(hojas1);
+			}
+		}
+		return hojasDelMismoTipo;
+	}
 }
